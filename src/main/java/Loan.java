2d2111aacfc2af3153f41c676ce657def6148463
Olenka Q <olenkaquispe85@gmail.com>
2021-07-27 19:15:50 -0400

    Added loan class

---
+++ src/main/java/Loan.java
@@ -0,0 +1,90 @@
+import java.lang.Math;
+
+public class Loan {
+    // data members
+    int loanAmount;
+    double interest;
+    int time;
+
+    // constructor
+    public Loan(int housePrice, int downPayment, float interest, int time) {
+        this.loanAmount = housePrice - downPayment;
+        this.interest = interest;
+        this.time = time;
+    }
+
+    public double calculateMonthlyPayments() {
+        //converts interest to decimal
+        double interestInDecimals = interest / 100;
+
+        //monthly interest rate
+        double monthlyInterestRate = interestInDecimals / 12;
+
+        //total number of monthly payments
+        int numOfPayments = time * 12;
+
+        /* calculates the monthly payment for a loan and stores it in variable result
+         * includes principal and interest only
+         * Amortized formula: M= P[r(1+r)^n/((1+r)^n)-1)]
+         * formula found in https://www.bankrate.com/calculators/mortgages/amortization-calculator.aspx
+         */
+        double result = this.loanAmount *(( (monthlyInterestRate) * Math.pow((1 + monthlyInterestRate), numOfPayments)) / ((Math.pow((1 + monthlyInterestRate), numOfPayments)) - 1));
+        // rounds results to the nearest two decimal places.
+        return Math.round(result * 100.0) / 100.0;
+    }
+
+    public double calculateMonthlyInterestPayments() {
+        double interestInDecimals = interest / 100;
+        double monthlyInterestRate = interestInDecimals / 12;
+        //Interest Payment = P * (r/n)
+        double result = this.loanAmount * monthlyInterestRate;
+        return Math.round(result * 100.0) / 100.0;
+    }
+
+    public double calculateMonthlyPrincipalPayments() {
+        double result = calculateMonthlyPayments() - calculateMonthlyInterestPayments();
+        return Math.round(result * 100.0) / 100.0;
+    }
+
+    public double calculateTotalLoanPayment() {
+         /*  N = Number of periods (number of monthly mortgage payments)
+            M = Monthly payment amount, calculated from last segment
+            P = Principal amount (the total amount borrowed, minus any down payments) */
+        //C = N * M
+        double result = this.calculateMonthlyPayments() * this.time * 12;
+        return result;
+    }
+
+    public double calculateTotalInterestPayment() {
+        double result = this.calculateTotalLoanPayment() - this.loanAmount;
+        return Math.round(result * 100.0) / 100.0;
+    }
+
+    // prints the amortized table for monthly payments
+    public void generateAmortizedPaymentsTable() {
+        //prints the table header
+        System.out.println("---------------------------------------------------------");
+        System.out.format("%5s %10s %10s %10s %15s", "Month","Monthly Payment", "Principal", "Interest", "Total balance");
+        System.out.println("\n-------------------------------------------------------");
+
+        double monthlyPayment = this.calculateMonthlyPayments();
+        double monthlyPrincipal = this.calculateMonthlyPrincipalPayments();
+        double monthlyInterest = this.calculateMonthlyInterestPayments();
+        double totalBalance = this.loanAmount -monthlyPrincipal;
+        int months  = this.time* 12;
+
+        //prints the loan information for each month;
+        for (int i = 1; i <= months; i++) {
+            System.out.format("%5s %10s %10s %10s %15s", i,Math.round(monthlyPayment* 100.0) /100.0, Math.round(monthlyPrincipal* 100.0) /100.0, Math.round(monthlyInterest* 100.0) /100.0, Math.round(totalBalance* 100.0) /100.0);
+            monthlyInterest = totalBalance * (this.interest / 1200);
+            monthlyPrincipal =monthlyPayment - monthlyInterest;
+            totalBalance  =  totalBalance - monthlyPrincipal;
+
+            System.out.println('\n');
+        }
+
+    }
+}
+
+
+
--- src/main/java/MortgageCalculator.java
+++ src/main/java/MortgageCalculator.java
@@ -1,12 +1,10 @@
 import java.util.Scanner;
-import java.lang.Math;
 
 class MortgageCalculator {
     
     public static void main(String[] args){
-//        System.out.println("Hello World!");
         int housePrice;
-        int downpayment;
+        int downPayment;
         float interest;
         int time;
 
@@ -19,46 +17,33 @@ class MortgageCalculator {
         //takes input from user & stores it in variable housePrice
         housePrice = input.nextInt();
 
-        System.out.println("Downpayment: ");
+        System.out.println("Down payment: ");
 
-        //takes input from user & stores it in variable downpayment
-        downpayment = input.nextInt();
+        //takes input from user & stores it in variable down payment
+        downPayment = input.nextInt();
 
         System.out.println("Interest rate: ");
         //takes input from user & stores it in variable interest
         interest = input.nextFloat();
 
         System.out.println("Mortgage period (Years): ");
-        //takes input from user & stores it in variable downpayment
+        //takes input from user & stores it in variable time
         time = input.nextInt();
 
         //close scanner
         input.close();
 
-        //For testing purposes
-        System.out.println("You entered the following information: " + '\n' + "House price: $" + housePrice + '\n' + "Downpayment: $"+ downpayment + '\n' + "Interest rate: " +interest+"%"+ '\n' + "Loan period: " +time +" years " + '\n' );
+        System.out.println("You entered the following information: " + '\n' + "House price: $" + housePrice + '\n' + "Down payment: $"+ downPayment + '\n' + "Interest rate: " +interest+"%"+ '\n' + "Loan period: " +time +" years " + '\n' );
 
-        //converts interest to decimal
-        float interestInDecimals = interest / 100 ;
+        //creates a new loan object
+        Loan newLoan = new Loan(housePrice, downPayment,interest, time);
+        //prints loan calculations
+        System.out.println("Total Monthly Payment (includes principal & interest): $ " + newLoan.calculateMonthlyPayments()
+                + "\nMonthly interest Payment: $"+newLoan.calculateMonthlyInterestPayments() +
+                "\nMonthly principal payment: $" + newLoan.calculateMonthlyPrincipalPayments() +
+                "\nTotal Loan payment: $" + newLoan.calculateTotalLoanPayment() + "\nTotal interest: $ " + newLoan.calculateTotalInterestPayment());
 
-        //monthly interest rate
-        float monthlyInterestRate = interestInDecimals/12;
-
-        // calculates final loan principal
-        double loanPrincipal = housePrice - downpayment;
-
-        //total number of monthly payments
-        int numOfPayments = time * 12;
-
-        /** calculates the monthly payment for a loan and stores it in variable result
-        * includes principal and interest only
-        * Amortized formula: P x (r / n) x (1 + r / n)^n(t)] / (1 + r / n)^n(t) - 1
-        * formula found in https://www.thebalance.com/calculate-mortgage-315668
-        */
-       double result =  (loanPrincipal * (monthlyInterestRate) * Math.pow((1+ monthlyInterestRate), numOfPayments))/ ((Math.pow((1+ monthlyInterestRate),360)) - 1);
-       // rounds results to the nearest two decimal places.
-       double roundedResult = Math.round(result * 100.0)/100.0;
-
-        System.out.println("You will be paying $" + roundedResult);
+        //returns the amortized Schedule table
+        newLoan.generateAmortizedPaymentsTable() ;
     }
 }
